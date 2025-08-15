d796c192e392901d822da9753366cfac9482fe68
Matthew Malyk <matthew.pm.dev@gmail.com>
2025-08-15 05:42:25 +0000

    add script to wait for jenkins config complete and restart container

--- jenkins/scripts/install-build-tools.sh
+++ jenkins/scripts/install-build-tools.sh
@@ -2,6 +2,7 @@
 apt-get update -y
 
 apt-get install -y curl
+
 curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
 apt-get install -y nodejs
 
--- terraform/scripts/jenkins_bootstrap.sh
+++ terraform/scripts/jenkins_bootstrap.sh
@@ -12,3 +12,25 @@ aws s3 cp "s3://${bucket_name}/jenkins/" "/usr/local/bin/init-scripts/" --recurs
 chmod -R +x /usr/local/bin/init-scripts/
 
 sudo -u jenkins docker compose up -d
+
+## scuffed script to wait for container config
+## until this can be replaced with ansible
+FLAG_PATH=/var/jenkins_home/init-scripts-done.flag
+CONTAINER_NAME=jenkins
+TIMEOUT=120
+INTERVAL=2
+ELAPSED=0
+
+while ! sudo -u jenkins docker exec "$CONTAINER_NAME" test -f "$FLAG_PATH"; do
+    sleep $INTERVAL
+    ELAPSED=$((ELAPSED + INTERVAL))
+    
+    if [ $ELAPSED -ge $TIMEOUT ]; then
+        echo "Timeout waiting for init scripts to complete in container $CONTAINER_NAME"
+        exit 1
+    fi
+done
+
+echo "Init scripts completed. Restarting container..."
+
+sudo -u jenkins docker restart jenkins
