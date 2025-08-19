b25b89967611143a6cade597959cd8ba09a2ba49
Matthew Malyk <matthew.pm.dev@gmail.com>
2025-08-19 10:43:19 +0000

    clean up some unused files

--- jenkins/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Read secrets into environment variables
-export ADMIN_USERNAME=$(cat /run/secrets/admin_username)
-export ADMIN_PASSWORD=$(cat /run/secrets/admin_password)
-
-# Start Jenkins (main process)
-exec /usr/bin/tini -- /usr/local/bin/jenkins.sh "$@"
